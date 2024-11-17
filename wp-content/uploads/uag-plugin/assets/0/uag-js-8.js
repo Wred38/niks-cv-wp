37dcad3fdf038cea2e0aff2cf5edd89d8f412425
Wred38 <nik.javor7@gmail.com>
2024-11-18 00:05:32 +0100

    Initial commit

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-8.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	UAGBTimelineClasses( {"block_id":"4c2b7159","timelinAlignment":"left","timelinAlignmentTablet":"left","timelinAlignmentMobile":"left"}, '.uagb-block-4c2b7159' );
+});
+window.addEventListener("resize", function(){
+	UAGBTimelineClasses( {"block_id":"4c2b7159","timelinAlignment":"left","timelinAlignmentTablet":"left","timelinAlignmentMobile":"left"}, '.uagb-block-4c2b7159' );
+});
+ });
